714a541e086cfc57412a1cd48df889b3cd7cd37f
Harsh Tiwari <harshtiwariai@gmail.com>
2023-01-10 18:25:02 +0530

    refactor: project setup with airflow standalone

--- dags/sample_dag/practice_dag.py
+++ dags/sample_dag/practice_dag.py
@@ -1,14 +1,35 @@
 
 from airflow import DAG
 
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
+
 from datetime import datetime
+from dotenv import load_dotenv
+
+
+from tasks.create_dummy_schema_task import create_dummy_schema
+
+load_dotenv()
 
 with DAG(
-    "practice_dag",
+    "slack_sql_integration_dag",
     start_date=datetime(2023, 1, 1),
     schedule_interval='@daily',
     catchup=False,
 ) as dag:
 
-    pass
+    initialization_task = PythonOperator(
+        task_id='initialization_task',
+        python_callable=create_dummy_schema
+    )
+
+
+    t1 = BashOperator(
+        task_id="print_date",
+        bash_command="date",
+    )
+
+
+    initialization_task >> t1
     
---
+++ dags/tasks/create_dummy_schema_task.py
@@ -0,0 +1,15 @@
+
+# from integrations.db_integration import DBIntegration
+
+def create_dummy_schema():
+    
+    # Create a cursor object
+
+    table_name = "harsh"
+    columns = "id SERIAL PRIMARY KEY, num INT, square_num INT"
+
+    print("Here")
+
+    # Execute a SQL query
+    # DBInstance.executeSQLStatement(f"CREATE TABLE IF NOT EXISTS {table_name} ({columns})")
+
